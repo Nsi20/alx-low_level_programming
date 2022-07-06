918ef787867913670a3b46a26c7f6a2342237e0b
Nsi20 <nsidibedanieldaniel622@gmail.com>
2022-07-06 15:29:03 -0700

    100 times table c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,6 +31,12 @@ void print_times_table(int n)
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
 			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
 			if (b < n)
 			{
 				_putchar(',');
