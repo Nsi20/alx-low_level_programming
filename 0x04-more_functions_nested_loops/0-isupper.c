8b6f1e8df9a7e72dfb26231fe542663ab2e0856e
Nsi20 <nsidibedanieldaniel622@gmail.com>
2022-07-08 05:42:21 -0700

    0 isupper c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a letter is upper
+ * @x: The number to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+
+}
