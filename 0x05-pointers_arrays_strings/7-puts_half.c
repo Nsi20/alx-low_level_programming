ed79e50dce5ee4190bdfef3556322ebf4cf7824a
Nsi20 <nsidibedanieldaniel622@gmail.com>
2022-07-12 17:57:01 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,33 +1,21 @@
 #include "main.h"
 
 /**
- * puts_half - prints a string
- * @str: pointer
- * Return: void
+ * puts_half - prints the second half of the string
+ * @str: the string reference
+ * Return: 0
  */
+
 void puts_half(char *str)
+
 {
-	int len, n, i;
-
-	len = 0;
-
-	while (str[len] != '\0')
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		len++;
-	}
-	if (len % 2 == 0)
-	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else if (len % 2)
-	{
-		for (n = (len - 1) / 2; n < len - 1; n++)
-		{
-			_putchar(str[n + 1]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
