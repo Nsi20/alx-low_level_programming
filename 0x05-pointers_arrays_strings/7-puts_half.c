ed7bd06ae024cad37a397a4cdc08e6f11a27f9a3
Nsi20 <nsidibedanieldaniel622@gmail.com>
2022-07-13 07:24:12 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,24 +9,11 @@
 void puts_half(char *str)
 
 {
-	int len, n, i;
-
-	len = 0;
-
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	if (len % 2 == 0)
-	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-        else if (len % 2)
-	{
-		for (n = (len - 1) / 2; n < len - 1; n++)
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
