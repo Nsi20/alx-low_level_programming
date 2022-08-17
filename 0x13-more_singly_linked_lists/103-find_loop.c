9452fc9198108ac9be576017f37630b8515430aa
Nsi20 <nsidibedanieldaniel622@gmail.com>
2022-08-16 18:29:09 -0700

    hot fix

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,24 +11,22 @@ listint_t *find_listint_loop(listint_t *head)
 	listint_t *slow = head;
 	listint_t *fast = head;
 
-	if (!head)
-		return (NULL);
-
-	while (slow && fast && fast->next)
-	{
-		fast = fast->next->next;
-		slow = slow->next;
-		if (fast == slow)
-		{
-			slow = head;
-			while (slow != fast)
-			{
-				slow = slow->next;
-				fast = fast->next;
-			}
-			return (fast);
-		}
-	}
-
+if (!head)
 	return (NULL);
+while (a && b && b->next)
+{
+	b = b->next->next;
+	a = a->next;
+	if (b == a)
+	{
+		a = head;
+		while (a != b)
+		{
+			a = a->next;
+			b = b->next;
+		}
+		return (b);
+	}
+}
+return (NULL);
 }
