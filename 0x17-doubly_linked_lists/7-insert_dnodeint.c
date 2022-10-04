cd94dc2721abb9a5f09dc28cc8879b699e5f7f98
Nsi20 <106813645+Nsi20@users.noreply.github.com>
2022-10-04 03:40:15 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,12 +1,43 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
-* insert_dnodeint_at_index - inserts a node at a given index
-* @h: the pointer to the head
-* @idx: where the node is being inserted
-* @n: what's being inserted
-* Return: The address of the new node, or NULL on failure
-*/
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the beginning of the linked list
+ * @idx: index at which to insert the new node
+ * @n: data to enter into new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new, *next, *current;
+	unsigned int i;
 
-}
+	if (h == NULL)
+		return (NULL);
+	if (idx != 0)
+	{
+		current = *h;
+		for (i = 0; i < idx - 1 && current != NULL; i++)
+			current = current->next;
+		if (current == NULL)
+			return (NULL);
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		next = *h;
+		*h = new;
+		new->prev = NULL;
+	}
+	else
+	{
+		new->prev = current;
+		next = current->next;
+		current->next = new;
+	}
