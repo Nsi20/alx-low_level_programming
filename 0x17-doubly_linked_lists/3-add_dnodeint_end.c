523651f8bad0b187c90c577941261b7767f51db1
Nsi20 <106813645+Nsi20@users.noreply.github.com>
2022-10-04 03:09:16 +0100

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,66 +1,31 @@
 #include "lists.h"
-
 /**
-<<<<<<< HEAD
- * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
- * @head: pointer to the first element of the list.
- * @head: pointer to the first element of the list.
- * Return: address of the new element, or NULL if it failed
- **/
+* add_dnodeint_end - adds an element to the end of the list
+* @head: pointer to the pointer of the head
+* @n: const int n
+* Return: the address of the new element, NULL if failed
+*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *aux = *head;
-=======
- * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
- * @head: A pointer to the head of the dlistint_t list.
- * @n: The integer for the new node to contain.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
- */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
-{
-	dlistint_t *new, *last;
->>>>>>> 8640807f291fab50e46094faac4736690e23b5df
+	dlistint_t *new, *end;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (!head)
 		return (NULL);
-<<<<<<< HEAD
-	new->n = n;
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
 	new->next = NULL;
-
-	if (aux)
-	{
-		while (aux->next)
-			aux = aux->next;
-		new->prev = aux;
-		aux->next = new;
-	}
+	new->prev = NULL;
+	new->n = n;
+	if (!*head)
+		*head = new;
 	else
 	{
-		*head = new;
-		new->prev = NULL;
+		end = *head;
+		while (end->next != NULL)
+			end = end->next;
+			end->next = new;
+			new->prev = end;
 	}
-
-=======
-
-	new->n = n;
-	new->next = NULL;
-
-	if (*head == NULL)
-	{
-		new->prev = NULL;
-		*head = new;
-		return (new);
-	}
-
-	last = *head;
-	while (last->next != NULL)
-		last = last->next;
-	last->next = new;
-	new->prev = last;
-
->>>>>>> 8640807f291fab50e46094faac4736690e23b5df
 	return (new);
 }
